857ce6588cd9a3a824c1c371a321ec74e19500fa
JackPu <kakashjack@gmail.com>
2017-04-18 23:40:39 +0800

    add new image

--- index.vr.js
+++ index.vr.js
@@ -6,13 +6,14 @@ import {
   Pano,
   Text,
   View,
+  Image,
 } from 'react-vr';
 
 export default class vrexamples extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('vrcase01.jpg')}/>
+        <Pano source={asset('vrcase02.jpg')}/>
         <Text
           style={{
             backgroundColor: '#777879',
@@ -26,6 +27,17 @@ export default class vrexamples extends React.Component {
           }}>
           hello
         </Text>
+        <Image
+          style={{
+            width: 1,
+            height: 1,
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            transform: [{translate: [0, 0, -3]}],
+          }}
+          source={require('./static_assets/Iron-Man.png')}
+        />
       </View>
     );
   }
